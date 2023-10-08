fd2ebba54aa879252722f2f97c7d667de89df1a4
Crêncio Mateus Sitoe <113183818+sitcren@users.noreply.github.com>
2023-10-09 01:47:18 +0200

    Header
    Working with the header

--- ver_notas_aluno.php
+++ ver_notas_aluno.php
@@ -16,33 +16,33 @@ try {
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ver notas</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ver notas</title>
 
 
-    <!-- Link Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
+        <!-- Link Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    </head>
 
-<body>
+    <body>
 
-    <?php
+        <?php
         include 'navBar.php'
         ?>
 
 
-    <?php
+        <section class="container mt-5">
+        <?php
 
 
 
-    // Exibição dos resultados em uma tabela
-    echo "<table border='1'>
+        // Exibição dos resultados em uma tabela
+        echo "<table border='1' class='table'>
 <tr>
 <th>Disciplina</th>
 <th>Nota 1</th>
@@ -51,31 +51,31 @@ try {
 <th>Média</th>
 </tr>";
 
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        // Calcular a média
-        $media = ($row['nota1'] + $row['nota2'] + $row['nota3']) / 3;
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            // Calcular a média
+            $media = ($row['nota1'] + $row['nota2'] + $row['nota3']) / 3;
 
-        echo "<tr>
+            echo "<tr>
     <td>{$row['disciplina']}</td>
     <td>{$row['nota1']}</td>
     <td>{$row['nota2']}</td>
     <td>{$row['nota3']}</td>
     <td>{$media}</td>
     </tr>";
+        }
+
+        echo "</table>";
+    } catch (\PDOException $e) {
+        // Em caso de erro na consulta
+        echo "Erro: " . $e->getMessage();
     }
-
-    echo "</table>";
-} catch (\PDOException $e) {
-    // Em caso de erro na consulta
-    echo "Erro: " . $e->getMessage();
-}
-    ?>
+        ?>
+        </section>
 
 
-    <!-- Link 2 -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
-</body>
+        <!-- Link 2 -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+        </script>
+    </body>
 
-</html>
+    </html>
