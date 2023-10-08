130af56c2e992500f8614bcf9850f223414620c0
Crêncio Mateus Sitoe <113183818+sitcren@users.noreply.github.com>
2023-10-09 01:39:13 +0200

    Bootstrap
    Implementando Bootstrap na pagina

--- ver_notas_aluno.php
+++ ver_notas_aluno.php
@@ -16,44 +16,52 @@ try {
 
 ?>
 
-    <!DOCTYPE html>
-    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Ver notas</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ver notas</title>
 
-    <body>
 
-    </body>
+    <!-- Link Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
 
-    </html>
+<body>
 
-<?php
+    <?php
+        include 'navBar.php'
+        ?>
+
+
+    <?php
+
+
 
     // Exibição dos resultados em uma tabela
     echo "<table border='1'>
-        <tr>
-            <th>Disciplina</th>
-            <th>Nota 1</th>
-            <th>Nota 2</th>
-            <th>Nota 3</th>
-            <th>Média</th>
-        </tr>";
+<tr>
+<th>Disciplina</th>
+<th>Nota 1</th>
+<th>Nota 2</th>
+<th>Nota 3</th>
+<th>Média</th>
+</tr>";
 
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         // Calcular a média
         $media = ($row['nota1'] + $row['nota2'] + $row['nota3']) / 3;
 
         echo "<tr>
-            <td>{$row['disciplina']}</td>
-            <td>{$row['nota1']}</td>
-            <td>{$row['nota2']}</td>
-            <td>{$row['nota3']}</td>
-            <td>{$media}</td>
-          </tr>";
+    <td>{$row['disciplina']}</td>
+    <td>{$row['nota1']}</td>
+    <td>{$row['nota2']}</td>
+    <td>{$row['nota3']}</td>
+    <td>{$media}</td>
+    </tr>";
     }
 
     echo "</table>";
@@ -61,3 +69,13 @@ try {
     // Em caso de erro na consulta
     echo "Erro: " . $e->getMessage();
 }
+    ?>
+
+
+    <!-- Link 2 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
