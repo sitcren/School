1f9f24b781c34b0c67921a01024dbd1b9afbe339
Crêncio Mateus Sitoe <113183818+sitcren@users.noreply.github.com>
2023-10-09 02:03:50 +0200

    Done
    O sistema está fluindo

--- navBar.php
+++ navBar.php
@@ -1,47 +1,41 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <divm class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="cadastroAlunos.php">Cadastro Alunos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="cadastroNotas.php">Cadastro Notas</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown link
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="cadastroAlunos.php">Cadastro Alunos</a></li>
-                        <li><a class="dropdown-item" href="formulario_notas_professor.php">Adicionar Notas Professor</a>
-                        </li>
-                        <li><a class="dropdown-item" href="login.php">Login</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </divm>
-</nav>
+<?php
 
+if (isset($nomeUsuario)) {
+    include 'navBar.php';
+}
+
+
+// Inclua o arquivo de conexão
+include 'conexao.php';
+
+// Supondo que o ID do aluno está armazenado na sessão após o login
+$aluno_id = isset($_SESSION['aluno_id']) ? $_SESSION['aluno_id'] : '';
+
+// Consulta SQL para obter o nome do aluno
+$sql = "SELECT nome FROM alunos WHERE id = :aluno_id";
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':aluno_id', $aluno_id);
+$stmt->execute();
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// Obtenha o nome do aluno
+$nomeUsuario = isset($row['nome']) ? $row['nome'] : '';
+
+?>
 <header class="container mb-5">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">System</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="#">Notas</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="#">Alunos</a>
                     </li>
 
                     <form class="d-flex" role="search">
@@ -49,19 +43,24 @@
                         <button class="btn btn-outline-success" type="submit">Search</button>
                     </form>
                 </ul>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
+
+                <?php
+
+
+                echo '<li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            ' . $nomeUsuario . '
+        </a>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="cadastroNotas.php">Add Notas</a></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#">Log Out</a></li>
+        </ul>
+      </li>';
+                ?>
             </div>
         </div>
     </nav>
--- ver_notas_aluno.php
+++ ver_notas_aluno.php
@@ -2,8 +2,6 @@
 // Inclua o arquivo de conexão
 include 'conexao.php';
 
-// Supondo que você tenha um mecanismo de autenticação e o ID do aluno esteja disponível na sessão
-// Certifique-se de ajustar esta parte conforme o seu sistema de autenticação
 session_start();
 $aluno_id = $_SESSION['aluno_id'];
 
@@ -16,52 +14,51 @@ try {
 
 ?>
 
-    <!DOCTYPE html>
-    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Ver notas</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ver notas</title>
 
 
-        <!-- Link Bootstrap -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    </head>
+    <!-- Link Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
 
-    <body>
+<body>
 
-        <?php
-        include 'navBar.php'
+    <?php
+        // include 'navBar.php'
         ?>
 
 
-        <section class="container mt-5">
+    <section class="container mt-5">
         <?php
-
-
 
         // Exibição dos resultados em uma tabela
         echo "<table border='1' class='table'>
-<tr>
-<th>Disciplina</th>
-<th>Nota 1</th>
-<th>Nota 2</th>
-<th>Nota 3</th>
-<th>Média</th>
-</tr>";
+            <tr>
+                <th>Disciplina</th>
+                <th>Nota 1</th>
+                <th>Nota 2</th>
+                <th>Nota 3</th>
+                <th>Média</th>
+            </tr>";
 
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             // Calcular a média
             $media = ($row['nota1'] + $row['nota2'] + $row['nota3']) / 3;
 
             echo "<tr>
-    <td>{$row['disciplina']}</td>
-    <td>{$row['nota1']}</td>
-    <td>{$row['nota2']}</td>
-    <td>{$row['nota3']}</td>
-    <td>{$media}</td>
-    </tr>";
+                <td>{$row['disciplina']}</td>
+                <td>{$row['nota1']}</td>
+                <td>{$row['nota2']}</td>
+                <td>{$row['nota3']}</td>
+                <td>{$media}</td>
+              </tr>";
         }
 
         echo "</table>";
@@ -70,12 +67,13 @@ try {
         echo "Erro: " . $e->getMessage();
     }
         ?>
-        </section>
+    </section>
 
 
-        <!-- Link 2 -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-        </script>
-    </body>
+    <!-- Link 2 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
 
-    </html>
+</html>
